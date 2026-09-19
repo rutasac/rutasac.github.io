699d521b8611aa0d29b3ec11d32a3450bbf2f4ca
rutasac <rutasac@users.noreply.github.com>
2026-09-19 05:54:51 +0000

    Acceso directo instalable: manifest, service worker e iconos

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'rutas-v1';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
